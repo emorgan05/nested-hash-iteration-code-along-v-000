13334ecfadbf143d921c5d270a59f1340026c114
Emily Morgan <emorgan05@gmail.com>
2017-09-25 14:50:59 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -23,3 +23,16 @@ def remove_strawberry(contacts)
     end
   end
 end
+
+remove_strawberry(contacts = {
+   "Jon Snow" => {
+     name: "Jon",
+     email: "jon_snow@thewall.we",
+     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+   },
+   "Freddy Mercury" => {
+     name: "Freddy",
+     email: "freddy@mercury.com",
+     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+   }
+ })
